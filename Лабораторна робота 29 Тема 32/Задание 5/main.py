f5ba83e1d7d9f4efaf924e4761ee78695640cc94
Ruslan <101466292+CmpaHHik7675@users.noreply.github.com>
2025-10-26 23:46:37 +0100

    Delete Лабораторна робота 29 Тема 32 directory

--- Лабораторна робота 29 Тема 32/Задание 5/main.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = 'https://example.com'
-response = requests.get(url)
-
-soup = BeautifulSoup(response.text, 'html.parser')
-title = soup.title.text if soup.title else "Заголовок не знайдено"
-print("Заголовок сторінки:", title)
